281e46e1231544f28ac29cbc94f06d93b00f27da
ch-iv <icheppy101@gmail.com>
2023-05-15 20:05:10 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import "./styles.css";
+</script>
+<div class="max-w-3xl p-8 mx-auto m-0 height-[100%]">
+    <slot></slot>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<h1 class="font-anon-bold mt-8 mb-0 text-3xl text-yellow-500">The Physics Behind Badminton</h1>
+<h2 class="font-anon-italic mb-8 text-md text-orange-500">By Alvin, Danish and Ivan</h2>
+
+<div class="font-anon hyphens-auto my-4" lang="en">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur autem dicta distinctio eaque eum magnam nesciunt optio, quaerat quisquam repellat, rerum sapiente, similique vel. Asperiores deleniti distinctio eius ex, facere illum in laborum laudantium maiores neque nihil non nulla officia omnis perferendis placeat vel! Aperiam atque blanditiis commodi debitis delectus deleniti distinctio dolorem eaque et ex facilis illum in ipsa iste labore laboriosam neque obcaecati odio omnis pariatur porro provident quasi rem repudiandae sapiente vero vitae, voluptatem? Beatae consequatur dignissimos doloribus ducimus eum fugiat illo, porro quas quod sequi. Deserunt, eaque impedit incidunt iusto nam nemo quos tempora. Ab accusamus ad alias beatae corporis delectus deserunt ex explicabo facere facilis id illo ipsa ipsam, ipsum, iure iusto labore laborum magnam mollitia natus nisi placeat quae quam quas qui quibusdam quis quod rem repellat reprehenderit repudiandae rerum sapiente sequi soluta sunt vitae voluptatem. Doloribus esse est ipsum labore omnis sint vero.
+</div>
+
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut cupiditate deserunt error est id maxime minus, nostrum numquam odit officia quo saepe, sint tempora ut veniam voluptatibus. At, beatae commodi culpa, delectus deserunt ea ex obcaecati officiis ratione repellat rerum voluptas. Accusamus deleniti harum ipsam quos, ratione reiciendis veritatis!
+</div>
+
+<img class="my-8" src="./src/lib/images/1.jpg">
+
+<h2 class="font-anon-bold mb-6 mt-16 text-2xl text-orange-500"><span class="text-yellow-500 text-4xl mr-2">#</span>How the shuttlecock works</h2>
+
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, deleniti, quas. Adipisci alias aliquam aut autem commodi consectetur deleniti, dicta dolore dolorem expedita incidunt libero nesciunt non nulla officia quas quasi similique tempore. Adipisci debitis dolorum explicabo inventore ipsum itaque laboriosam magni, natus nisi non, quasi sunt ut veritatis vitae.
+</div>
+
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+    <ul class="list-disc ml-8">
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, veritatis!</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+    </ul>
+</div>
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cumque enim et ex explicabo laudantium nihil recusandae rem suscipit voluptas.
+</div>
+
+<h2 class="font-anon-bold mb-6 mt-16 text-2xl text-orange-500"><span class="text-yellow-500 text-4xl mr-2">#</span>How the racket works</h2>
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cumque enim et ex explicabo laudantium nihil recusandae rem suscipit voluptas.
+</div>
+
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+    <ul class="list-decimal ml-8">
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, veritatis!</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+    </ul>
+</div>
+
+<div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias amet blanditiis consequatur corporis dolorum excepturi facere hic ipsam, iste laborum laudantium magnam minima necessitatibus nihil, nulla placeat porro quasi quidem quis repellendus saepe similique soluta vero vitae? Accusantium adipisci alias aliquam animi architecto, atque beatae corporis doloremque eaque eligendi et ex libero maiores necessitatibus numquam, odio officia officiis perferendis quidem quod repellat sapiente sit totam, vitae voluptate voluptatem voluptatibus! Ab alias assumenda beatae consequatur dignissimos eos error et inventore nam natus, numquam provident quidem quisquam quo ratione repellendus ullam velit. Aperiam cupiditate dolor et eveniet fuga, fugit inventore iusto, laudantium, libero magnam magni nemo numquam qui quis rem repellat rerum sequi sit soluta suscipit ullam unde voluptatibus. Dicta excepturi ipsam minima nesciunt quis sit? A accusamus aliquid amet aut beatae blanditiis consectetur corporis delectus deleniti dicta distinctio dolore doloremque eius error et eveniet ex exercitationem facere illum impedit libero, nemo neque nesciunt nostrum nulla pariatur quis, reiciendis repellat repellendus saepe sequi ullam unde velit? Autem delectus deserunt distinctio id libero molestias praesentium repellendus sit suscipit tenetur? A culpa dicta distinctio dolorum enim eos eum exercitationem expedita explicabo harum libero, maiores, necessitatibus nemo nobis perferendis quis sint velit vero voluptate.
+</div>
