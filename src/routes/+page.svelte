f9d6e06029bacf6fbd208fcf89950ffc4fa65fc8
ch-iv <icheppy101@gmail.com>
2023-06-13 07:51:56 -0400

    partial add content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,57 @@
 </script>
 <div class="max-w-3xl p-8 mx-auto m-0 height-[100%]">
 
-<h1 class="font-anon-bold mt-8 mb-0 text-3xl text-yellow-500">The Physics Behind Badminton</h1>
+<h1 class="font-anon-bold mt-8 mb-0 text-3xl text-yellow-500">Physics Behind Badminton</h1>
 <h2 class="font-anon-italic mb-8 text-md text-orange-500">By Alvin, Danish and Ivan</h2>
 
 
 
+<h1 class="font-anon-bold mt-8 mb-0 text-2xl text-yellow-400">Physics behind the shuttlecock</h1>
+<h1 class="font-anon-bold mt-2 mb-0 text-xl text-yellow-400">Introduction</h1>
 
 <div class="font-anon hyphens-auto my-4" lang="en">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur autem dicta distinctio eaque eum magnam nesciunt optio, quaerat quisquam repellat, rerum sapiente, similique vel. Asperiores deleniti distinctio eius ex, facere illum in laborum laudantium maiores neque nihil non nulla officia omnis perferendis placeat vel! Aperiam atque blanditiis commodi debitis delectus deleniti distinctio dolorem eaque et ex facilis illum in ipsa iste labore laboriosam neque obcaecati odio omnis pariatur porro provident quasi rem repudiandae sapiente vero vitae, voluptatem? Beatae consequatur dignissimos doloribus ducimus eum fugiat illo, porro quas quod sequi. Deserunt, eaque impedit incidunt iusto nam nemo quos tempora. Ab accusamus ad alias beatae corporis delectus deserunt ex explicabo facere facilis id illo ipsa ipsam, ipsum, iure iusto labore laborum magnam mollitia natus nisi placeat quae quam quas qui quibusdam quis quod rem repellat reprehenderit repudiandae rerum sapiente sequi soluta sunt vitae voluptatem. Doloribus esse est ipsum labore omnis sint vero.
+    The badminton shuttlecock, also called a shuttle or birdie, is the projectile that is used in badminton. The shuttle makes badminton special and different from the rest of racket sports, where usually a ball is used as a projectile.
 </div>
+
+<div class="font-anon hyphens-auto my-4" lang="en">
+    Shuttlecock’s average dimensions:
+</div>
+
+<h1 class="font-anon-bold mt-2 mb-0 text-xl text-yellow-400">The Design Of THe Shuttlecock</h1>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        To understand the physics behind why the shuttlecock flips during flight, such that the cork points in the direction of the motion we first need to understand the terms centre of gravity and the pressure center.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        The Centre of gravity is a point from which the weight of a body or system may be considered to act. It depends upon the mass distribution over the body. As the mass is mostly concentrated in the cork, the centre of gravity is located in the cork.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        The pressure centre is the point within the object where the combined effect of the air resistance forces is considered to be concentrated. It's a way to simplify the complex interaction between the object and the air into a single point. In the case of a shuttlecock, it is located near the tip of the feathers (the skirt) where the cross-sectional area is the largest.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        Cg - the centre of gravity<br>
+        Cp - the centre of pressure
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        This image illustrates the flipping of the shuttlecock through the air.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        Using the concepts of Cg and Cp we can explain the flipping of the shuttlecock. As the shuttlecock flies through the air, the force of drag exerted on the shuttlecock is mostly exerted at the centre of pressure, causing a negative acceleration (deceleration). We can consider the cork (where the Cg is located) and the skirt (where the Cp is located) as two separate bodies for the sake of calculating their acceleration separately.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        Newton's second law states that Net force = mass acceleration . If we rearrange this equation to find the acceleration of a body we get Acceleration = Net Forcemass. So, The only force acting on the shuttlecock in the horizontal axis is aerodynamic drag. This means the net force will be equal to the drag force on the shuttlecock. As you just learnt the aerodynamic drag is experienced more at the centre of pressure. This means the net force (which will be negative as it is opposing the motion of the shuttlecock) will be greater at the centre of pressure (at the skirt) and hence the acceleration in the negative direction (deceleration) will also be greater for the skirt.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        The centre of gravity is located at the cork of the shuttlecock, meaning most of the mass is concentrated there. Using newtons second law again we can see that a larger mass will result in less acceleration for a given amount of force. So overall this means that during flight a birdie will experience a much greater deceleration at its skirt as compared to the cork, this results in the skirt moving behind the cork and hence the head of the shuttlecock flips and the cork faces forwards.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        This image illustrates the flipping of the shuttlecock through the air.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        This image illustrates the flipping of the shuttlecock through the air.
+    </div>
+    <div class="font-anon hyphens-auto my-4" lang="en">
+        This image illustrates the flipping of the shuttlecock through the air.
+    </div>
 
 <div class="font-anon hyphens-auto my-4 hyphens-auto" lang="en">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut cupiditate deserunt error est id maxime minus, nostrum numquam odit officia quo saepe, sint tempora ut veniam voluptatibus. At, beatae commodi culpa, delectus deserunt ea ex obcaecati officiis ratione repellat rerum voluptas. Accusamus deleniti harum ipsam quos, ratione reiciendis veritatis!
